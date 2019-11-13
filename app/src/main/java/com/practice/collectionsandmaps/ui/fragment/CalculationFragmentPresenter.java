ec85a7ea64c16e513a5db6f10de9cf2b4d6d9ae2
Sergii Kvartnyk <parashut131@gmail.com>
2019-11-13 22:12:09 +0200

    added observeOn and subscribeOn to flatMap

--- app/src/main/java/com/practice/collectionsandmaps/ui/fragment/CalculationFragmentPresenter.java
+++ app/src/main/java/com/practice/collectionsandmaps/ui/fragment/CalculationFragmentPresenter.java
@@ -8,7 +8,6 @@ import com.practice.collectionsandmaps.dto.TaskData;
 import com.practice.collectionsandmaps.models.suppliers.TasksSupplier;
 import com.practice.collectionsandmaps.models.workers.TimeCalculator;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 
@@ -16,11 +15,9 @@ import javax.inject.Inject;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
+import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
@@ -33,35 +30,33 @@ public class CalculationFragmentPresenter implements CalculationFragmentContract
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Inject
-    public CalculationFragmentPresenter(CalculationFragmentContract.FragmentView view, TasksSupplier tasksSupplier, TimeCalculator timeCalculator){
+    public CalculationFragmentPresenter(CalculationFragmentContract.FragmentView view, TasksSupplier tasksSupplier, TimeCalculator timeCalculator) {
         isValid = true;
         this.view = view;
         this.tasksSupplier = tasksSupplier;
         this.timeCalculator = timeCalculator;
     }
 
-    public int getCollectionsCount(){
+    public int getCollectionsCount() {
         return tasksSupplier.getCollectionsCount();
     }
 
-
-
-    public void startCalculation(String amountOfElements, String amountOfThreads){
+    public void startCalculation(String amountOfElements, String amountOfThreads) {
         isValid = true;
-        if(amountOfElements.isEmpty()) {
+        if (amountOfElements.isEmpty()) {
             view.setElementsError(view.getStringFromResources(R.string.elements_must_not_be_empty));
             isValid = false;
-        } else if("0".equals(amountOfElements)){
+        } else if ("0".equals(amountOfElements)) {
             view.setElementsError(view.getStringFromResources(R.string.enter_elements));
             isValid = false;
         } else {
             view.setElementsError(null);
         }
 
-        if(amountOfThreads.isEmpty()){
+        if (amountOfThreads.isEmpty()) {
             view.setThreadsError(view.getStringFromResources(R.string.threads_must_not_be_empty));
             isValid = false;
-        } else if("0".equals(amountOfThreads)){
+        } else if ("0".equals(amountOfThreads)) {
             view.setThreadsError(view.getStringFromResources(R.string.enter_threads));
             isValid = false;
         } else {
@@ -74,34 +69,44 @@ public class CalculationFragmentPresenter implements CalculationFragmentContract
             view.showProgress();
             final int threads = Integer.parseInt(amountOfThreads);
             final int elements = Integer.parseInt(amountOfElements);
-            final List<TaskData> tasks = tasksSupplier.getTasks();
-            compositeDisposable.add(Observable.fromIterable(new ArrayList<>(tasks))
-            .subscribeOn(Schedulers.from(Executors.newFixedThreadPool(threads)))
-            .flatMap((Function<TaskData, ObservableSource<CalculationResult>>) taskData -> {
-                Log.d("MyLog", "in CalculationFragmentPresenter startCalculation() at flatMap() thread = " + Thread.currentThread().getName());
-                taskData.fill(elements);
-                timeCalculator.execAndSetupTime(taskData);
-                return Observable.just(taskData.getResult());
-            })
-            .observeOn(AndroidSchedulers.mainThread())
-            .doFinally(() -> {
-                compositeDisposable.clear();
-                if (view != null && view.isCheckedBtn()) {
-                    view.setBtnChecked(false);
-                    view.showToast(view.getStringFromResources(R.string.calculation_finished));
-                }
-
-            })
-            .subscribe(calculationResult -> {
-                if (view != null) {
-                    view.setupResult(calculationResult);
-                }
-            }));
+            final Scheduler pool = Schedulers.from(Executors.newFixedThreadPool(threads));
+            compositeDisposable.add(Observable.fromIterable(tasksSupplier.getTasks())
+                    .flatMap((Function<TaskData, ObservableSource<CalculationResult>>) taskData -> {
+                        Log.d("MyLogThreads", "in CalculationFragmentPresenter startCalculation() at flatMap() thread = "
+                                + Thread.currentThread().getName());
+                        return Observable.just(taskData)
+                                .subscribeOn(pool)
+                                .map(taskData1 -> {
+                                    Log.d("MyLogThreads", "in CalculationFragmentPresenter startCalculation() at Map() thread = "
+                                            + Thread.currentThread().getName());
+                                    taskData1.fill(elements);
+                                    timeCalculator.execAndSetupTime(taskData1);
+                                    return taskData1.getResult();
+                                })
+                                .observeOn(AndroidSchedulers.mainThread());
+                    })
+                    .doFinally(() -> {
+                        Log.d("MyLogThreads", "in CalculationFragmentPresenter startCalculation() at doFinally() thread = "
+                                + Thread.currentThread().getName());
+                        stopCalculation(false);
+                        if (view != null) {
+                            view.setBtnChecked(false);
+                            view.showToast(view.getStringFromResources(R.string.calculation_finished));
+                        }
+                    })
+                    .subscribe(calculationResult -> {
+                        Log.d("MyLogThreads", "in CalculationFragmentPresenter startCalculation() at subscribe() where result are settled thread = "
+                                + Thread.currentThread().getName());
+                        if (view != null) {
+                            Log.d("MyLog", "in CalculationFragmentPresenter setupResult()" + calculationResult.getTimeForTask());
+                            view.setupResult(calculationResult);
+                        }
+                    }));
         }
     }
 
     public void stopCalculation(boolean showMsg) {
-        if(compositeDisposable.size() == 0){
+        if (compositeDisposable.size() == 0) {
             return;
         }
         compositeDisposable.clear();
